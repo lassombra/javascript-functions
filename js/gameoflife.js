5409c327a41f397741b5313df6e046e658ed148b
Brian Engelhardt <bengelhardt@homfurniture.com>
2022-11-08 13:08:11 -0600

    Project complete

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,27 +1,109 @@
-function seed() {}
+function seed() {
+  return [...arguments];
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return x === j && y === k;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return !!this.find(a => same(a, cell));
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  return contains.call(state, cell) ? '\u25A3' : '\u25A2';
+};
 
-const corners = (state = []) => {};
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return {
+      topRight: [0,0],
+      bottomLeft: [0,0]
+    }
+  }
+  const [x,y] = state.reduce((axis, cell) => {
+    axis[0].push(cell[0]);
+    axis[1].push(cell[1]);
+    return axis;
+  }, [[], []]);
+  x.sort();
+  y.sort();
+  return {
+    topRight: [x.slice(-1)[0], y.slice(-1)[0]],
+    bottomLeft: [x[0], y[0]]
+  }
+};
+function iterateCells(bounds = {topRight: [0, 0], bottomLeft: [0, 0]},
+                      cellFn = () => {/* noop */},
+                      rowFn = () => { /* noop */},
+                      postRowFn = () => {/* noop */}) {
+  for (let y = bounds.topRight[1]; y >= bounds.bottomLeft[1]; y--) {
+    let rowVal = rowFn();
+    for (let x = bounds.bottomLeft[0]; x <= bounds.topRight[0]; x++) {
+      cellFn([x,y], rowVal);
+    }
+    postRowFn(rowVal);
+  }
+}
+const printCells = (state) => {
+  const myCorners = corners(state);
+  const rows = [];
+  iterateCells(myCorners, (cell, row) => row.push(printCell(cell, state)), () => [],
+      row => rows.push(row.join(' ') + '\n'));
+  return rows.join('');
+};
 
-const printCells = (state) => {};
+const getNeighborsOf = ([x, y]) => {return [
+    [x-1, y+1], [x, y+1], [x+1, y+1],
+    [x-1, y], /*[x,y],*/ [x+1, y],
+    [x-1, y-1], [x, y-1], [x+1, y-1]
+]};
 
-const getNeighborsOf = ([x, y]) => {};
+const getLivingNeighbors = (cell, state) => {
+  const living = contains.bind(state);
+  return getNeighborsOf(cell).filter(living);
+};
 
-const getLivingNeighbors = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  const neighbors = getLivingNeighbors(cell, state).length;
+  return neighbors === 3 ||  contains.call(state, cell) && neighbors === 2;
+};
 
-const willBeAlive = (cell, state) => {};
+function expandCornersBy(existingCorners, factor) {
+  return {
+    topRight: [existingCorners.topRight[0] + factor,
+      existingCorners.topRight[1] + factor],
+    bottomLeft: [existingCorners.bottomLeft[0] - factor,
+        existingCorners.bottomLeft[1] - factor]
+  }
+}
+const calculateNext = (state) => {
+  const newState = [];
+  iterateCells(expandCornersBy(corners(state), 1), cell => {
+    if (willBeAlive(cell, state)) {
+      newState.push(cell);
+    }
+  }, () => {/*noop*/
+  })
+  return newState;
+};
 
-const calculateNext = (state) => {};
+const iterate = (state, iterations) => {
+  const states = [state];
+  let currentState = state;
+  for (let i = 0; i < iterations; i++) {
+    currentState = calculateNext(currentState);
+    states.push(currentState);
+  }
+  return states;
+};
 
-const iterate = (state, iterations) => {};
-
-const main = (pattern, iterations) => {};
+const main = (pattern, iterations) => {
+  let  states = iterate(startPatterns[pattern], iterations);
+  states = states.map(printCells);
+  console.log(states.join('\n'));
+};
 
 const startPatterns = {
     rpentomino: [
@@ -50,12 +132,12 @@ const startPatterns = {
     ]
   };
   
-  const [pattern, iterations] = process.argv.slice(2);
+  const [selectedPattern, numIterations] = process.argv.slice(2);
   const runAsScript = require.main === module;
   
   if (runAsScript) {
-    if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
-      main(pattern, parseInt(iterations));
+    if (startPatterns[selectedPattern] && !isNaN(parseInt(numIterations))) {
+      main(selectedPattern, parseInt(numIterations));
     } else {
       console.log("Usage: node js/gameoflife.js rpentomino 50");
     }
